80fb5ca37041fef1a1845e95e976d8f7b0480f38
mattiasen1 <mattiasen2@gmail.com>
2020-01-13 14:46:54 +0100

    Mina tests

--- Tests/Quaternion/test_balt.py
+++ Tests/Quaternion/test_balt.py
@@ -1,17 +1,20 @@
-from sympy import Quaternion, symbols
+from sympy import Quaternion, symbols, sqrt
 import unittest
+import math
 
 w, x, y, z = symbols('w:z')
 
 class TestQuaternion(unittest.TestCase):
   
   def test_inverse(self):
+    #Test inverse of quaternions. Inverse is defined as Quaternion(w, -x, -y, -z) / (w**2 + x**2 + y**2 + z**2). Also tested with pow(-1) since this is equivalent to the inverse.
     q = Quaternion(w, x, y, z)
     self.assertEqual(q.inverse(), Quaternion(w, -x, -y, -z) / (w**2 + x**2 + y**2 + z**2))
-    self.assertNotEqual(q.inverse(), Quaternion(w, -x, -y, -z) /w**2 + x**2 + y**2 + z**3)
+    self.assertNotEqual(q.inverse(), Quaternion(w, -x, -y, -z) / (w**2 + x**2 + y**2 + z**3))
     self.assertEqual(q.inverse(), q.pow(-1))
   
   def test_quaternion_initialize(self):
+    #Test if initializition of quaternions work. If correct errors are raised when wrong inputs are put in to the function.
     q = Quaternion(w, x, y, z)
     with self.assertRaises(NameError):
       q = Quaternion(w, x, y, i)
@@ -19,7 +22,20 @@ class TestQuaternion(unittest.TestCase):
       q = Quaternion([1,4,5])
     
     self.assertEqual(q + q, Quaternion(w*2, x*2, y*2, z*2))
+  
+  def test_norm(self):
+    #By definition ||q|| = sqrt(w^2 + x^2 + y^2 + z^2). Tested from definition with both sqrt and raise to the power 0.5.
+    q = Quaternion(w, x, y, z)
+    self.assertEqual(q.norm(), sqrt(w**2 + x**2 + y**2 + z**2))
+    self.assertEqual(q.norm(), (w**2 + x**2 + y**2 + z**2)**0.5)
 
+  def test_add(self):
+    #Testing if add function works. And also that addition without the add function works as well. Testing with negative inputs as well.
+    q = Quaternion(w, x, y, z)
+    self.assertEqual(q + q, Quaternion(2*w, 2*x, 2*y, 2*z))
+    self.assertEqual(q.add(q), (q + q))
+    self.assertEqual(q.add(-q), (q - q))
+    self.assertEqual((-q).add(-q), (-q - q))
 
 if __name__ == "__main__":
   unittest.main()
